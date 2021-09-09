bc66675301a5415d91ba65fe964f77114f7358a8
Soumil491 <soumilsarkar47007@gmail.com>
2021-09-09 12:37:21 +0530

    latest version 2

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.get('/addProduct', (req, res) => {
     res.render('addProduct.ejs');
 })
 
-app.post('/postProducts', async (req, res) => {
+app.post('/postProduct', async (req, res) => {
+    console.log(req.body.id);
     let newProduct = {
         "id": parseInt(req.body.id),
         "title": req.body.title,
@@ -45,10 +46,12 @@ app.post('/postProducts', async (req, res) => {
         }
     }
     try {
+        console.log(newProduct);
         const response = await axios.get('https://fakestoreapi.com/products');
         result = response.data.slice(0, 10);
         products = [...result];
         products.push(newProduct);
+        console.log(newProduct);
         res.render('index.ejs', { data: products });
     }
     catch (error) {
